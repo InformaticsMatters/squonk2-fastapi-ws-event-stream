24c5736a1fda30885851c374aab237bdcef00cdb
Alan Christie <alan.christie@matildapeak.com>
2025-02-11 18:25:39 +0000

    fix: More realistic app

--- app/app.py
+++ app/app.py
@@ -1,19 +1,31 @@
 """The entrypoint for the Squonk2 FastAPI WebSocket service."""
 
-from fastapi import FastAPI
+import uuid
+
+from fastapi import FastAPI, HTTPException, WebSocket, status
 
 # Public (event-stream) and internal (REST) services
-app_es = FastAPI()
+app_public = FastAPI()
 app_internal = FastAPI()
+
+_ES_UUID_SET: set = set()
 
 
 # Endpoints for the 'public-facing' event-stream web-socket API ------------------------
 
 
-@app_es.get("/event-stream/")
-def event_stream():
-    """Read from the event-stream."""
-    return {"message": "MESSAGE"}
+@app_public.websocket("/event-stream/{es_id}")
+async def event_stream(websocket: WebSocket, es_id: str):
+    """The websocket handler for the event-stream."""
+    if not es_id in _ES_UUID_SET:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"EventStream {es_id} is not known",
+        )
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
 
 
 # Endpoints for the 'internal' event-stream management API -----------------------------
@@ -22,10 +34,18 @@ def event_stream():
 @app_internal.post("/event-stream/")
 def post_es():
     """Create a new event-stream."""
-    return {"id": 1}
+    uuid_str = f"es-{uuid.uuid4()}"
+    _ES_UUID_SET.add(uuid_str)
+    return {"id": uuid_str}
 
 
 @app_internal.delete("/event-stream/{es_id}")
-def delete_es(es_id: int):
+def delete_es(es_id: str):
     """Destroys an existing event-stream."""
-    return {"message": f"Destroy ES {es_id}"}
+    if es_id not in _ES_UUID_SET:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"EventStream {es_id} is not known",
+        )
+    _ES_UUID_SET.remove(es_id)
+    return {}
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,5 @@
 # and the internal REST endpoint.
 # Done my simply launching two uvicorn instances in parallel.
 echo "+> Launching uvicorn..."
-uvicorn app.app:app_p --reload --host 0.0.0.0 --port 8080 & \
-    uvicorn app.app:app_i --reload --host 0.0.0.0 --port 8081
+uvicorn app.app:app_public --reload --host 0.0.0.0 --port 8080 & \
+    uvicorn app.app:app_internal --reload --host 0.0.0.0 --port 8081
